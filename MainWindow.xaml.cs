498140a3853ebf38ecc41634d0fa498a67901194
Jack Sanders <jsanders4@sheffield.ac.uk>
2022-07-06 20:59:30 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace CKChronicler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
