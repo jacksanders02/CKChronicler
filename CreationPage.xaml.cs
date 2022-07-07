8c337148ccdbee51385b7acee52f0ae2fb917501
Jack Sanders <jacksanders404@gmail.com>
2022-07-08 00:17:59 +0100

    WIP Chronicle creation - can input a name and have it displayed in a preview

--- CreationPage.xaml.cs
+++ CreationPage.xaml.cs
@@ -24,4 +24,13 @@ public partial class CreationPage : Page
     {
         InitializeComponent();
     }
+
+
+    private void UpdateCharPreviewText(object sender, TextChangedEventArgs e) {
+        string rank = CharRank.Text.Length > 0 ? CharRank.Text : "";
+        string name = CharName.Text.Length > 0 ? " " + CharName.Text : "";
+        string title = CharTitle.Text.Length > 0 ? " of " + CharTitle.Text : "";
+
+        CharPreview.Text = $"{rank}{name}{title}";
+    }
 }
