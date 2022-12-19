e51ecda098b33fd97f352ad114b4b71cb899616d
Jack Sanders <jacksanders404@gmail.com>
2022-12-19 00:21:16 +0000

    Updated char details logic

--- CharDetails.xaml.cs
+++ CharDetails.xaml.cs
@@ -1,17 +1,29 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections;
+using System.Windows.Controls;
 
 namespace CKChronicler; 
 
 public partial class CharDetails : Page {
+    private string _rank = "";
+    private string _name = "";
+    private string _title = "";
+    
     public CharDetails() {
         InitializeComponent();
     }
     
     private void UpdateCharPreviewText(object sender, TextChangedEventArgs e) {
-        string rank = CharRank.Text.Length > 0 ? CharRank.Text : "";
-        string name = CharName.Text.Length > 0 ? " " + CharName.Text : "";
-        string title = CharTitle.Text.Length > 0 ? " of " + CharTitle.Text : "";
+        _rank = CharRank.Text;
+        _name = CharName.Text;
+        _title = CharTitle.Text;
 
-        CharPreview.Text = $"{rank}{name}{title}";
+        string rankNameSpacing = _rank.Length > 0 && _name.Length > 0 ? " " : "";
+        string nameTitleSpacing = (_name.Length > 0 || _rank.Length > 0) && _title.Length > 0 ? " of " : "";
+
+        CharPreview.Text = $"{_rank}{rankNameSpacing}{_name}{nameTitleSpacing}{_title}";
+    }
+
+    protected string[] GetCharDetails() {
+        return new string[] { _rank, _name, _title };
     }
 }
