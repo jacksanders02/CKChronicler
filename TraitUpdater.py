c17f2e74baa6ec2ece18c97e7d717fd14ed62c1e
Jack Sanders <jacksanders404@gmail.com>
2022-12-20 01:33:21 +0000

    Added personality trait parsing

--- TraitUpdater.py
+++ TraitUpdater.py
@@ -4,6 +4,19 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+# Website I'm scraping the trait data from
+TRAITS_PAGE = requests.get("https://ck3.paradoxwikis.com/Traits")
+
+SOUP = BeautifulSoup(TRAITS_PAGE.content, "html.parser")
+
+# 'Normal' table names
+NORMALS = ["Congenital_traits", "Physical_traits", "Lifestyle_traits",
+           "Commander_traits", "Infamous_traits", "Coping_mechanisms",
+           "Dynasty_traits", "Decision_traits", "Event_traits",
+           "Unobtainable_traits"]
+
+PERSONALITY = SOUP.find(id="Personality_traits").find_next("tbody")
+
 
 # Takes a list of effects in HTML, and parses them to a dictionary
 def parse_effects(effects):
@@ -16,18 +29,36 @@ def parse_effects(effects):
         effects_list = [effects]
 
     for e in effects_list:
-        current_effect = { }
-        current_effect["effected_attr"] = ''.join(re.findall(r'[^0-9.\-−+%]',
-                                                             e.text)).strip()
+        if "mutually exclusive" in e.text.lower():
+            continue
 
         mag = e.find_all('b')
         pos = e.find_all('span')
 
-        current_effect["effect_magnitude"] = mag[0].text.strip().replace("−","-") if mag else ""
-        current_effect["positivity"] = pos[0]["class"][0].strip() if pos else ""
+        current_effect = {
+            "effected_attr": ''.join(re.findall(r'[^0-9.\-−+%]',
+                                                e.text)).strip(),
+            "effect_magnitude": mag[0].text.strip().replace("−",
+                                                            "-") if mag else "",
+            "positivity": pos[0]["class"][0].strip() if pos else ""
+        }
 
         effects_data.append(current_effect)
     return effects_data
+
+
+def get_heading_data(table):
+    # Get headings
+    heading_data = []
+
+    headings = table.find_next('tr').find_all('th')
+    for h in headings:
+        if h.has_attr("colspan"):
+            heading_data.extend(
+                [f"{h.text.strip()}_{i}" for i in range(int(h["colspan"]))])
+        else:
+            heading_data.append(h.text.strip())
+    return heading_data
 
 
 # Parses a 'regular' table structure to a list of dictionaries (one per trait)
@@ -38,16 +69,7 @@ def regular_table(traits_table):
     for tc in to_clear:
         tc.extract()
 
-    # Get headings
-    heading_data = []
-
-    headings = traits_table.find_next('tr').find_all('th')
-    for h in headings:
-        if h.has_attr("colspan"):
-            heading_data.extend(
-                [f"{h.text.strip()}_{i}" for i in range(int(h["colspan"]))])
-        else:
-            heading_data.append(h.text.strip())
+    heading_data = get_heading_data(traits_table)
 
     # Find important indices
     icon_index = heading_data.index("Trait_0")
@@ -57,15 +79,13 @@ def regular_table(traits_table):
 
     description_index = heading_data.index("Description")
 
-    traits = []
+    table_traits = []
     rows = traits_table.find_all('tr')[1:]
     for row in rows:
         cols = row.find_all('td')
 
-        if len(cols) == 0:
-            print(row.prettify())
-
         row_data = []
+        # Parse all necessary data
         for i in [icon_index, name_index, effects_index, description_index]:
             td = cols[i]
             if i == effects_index:
@@ -77,30 +97,62 @@ def regular_table(traits_table):
             else:
                 row_data.append("")
 
-        traits.append({
+        table_traits.append({
             "name": row_data[1],
             "icon": row_data[0],
             "effects": row_data[2],
             "description": row_data[3]
         })
-    return traits
+    return table_traits
 
 
-# Website I'm scraping the trait data from
-TRAITS_PAGE = requests.get("https://ck3.paradoxwikis.com/Traits")
+# Handles weirdly-formatted personality traits table(s)
+def personality_traits(personality_table):
+    description_table = personality_table.find_next('tbody')
 
-soup = BeautifulSoup(TRAITS_PAGE.content, "html.parser")
+    heading_data = get_heading_data(personality_table)
+    description_headings = get_heading_data(description_table)
 
-# 'Normal' table names
-NORMALS = ["Congenital_traits", "Physical_traits", "Lifestyle_traits",
-           "Commander_traits", "Infamous_traits", "Coping_mechanisms",
-           "Dynasty_traits", "Decision_traits", "Event_traits",
-           "Unobtainable_traits"]
+    # Find important indices
+    icon_index = description_headings.index("Trait_0")
+    name_index = description_headings.index("Trait_1")
+    effects_index = [i for i, w in enumerate(heading_data) if "effect" in
+                     w.lower()][0]
+    desc_index = description_headings.index("Description")
+
+    table_traits = []
+    rows = personality_table.find_all('tr')[1:]
+    description_rows = description_table.find_all('tr')[1:]
+
+    # Need to zip properties and descriptions together as they are in
+    # separate tables for some reason
+    for (p_row, d_row) in zip(rows, description_rows):
+        p_cols = p_row.find_all('td')
+        d_cols = d_row.find_all('td')
+        trait_data = [p_cols[:len(heading_data) // 2], p_cols[len(heading_data) // 2:]]
+        descriptions = [d_cols[:len(description_headings) // 2], d_cols[len(description_headings) // 2:]]
+
+        for trait, desc in zip(trait_data, descriptions):
+            # Handle instances like compassionate, where a trait has rowspan=2
+            if len(trait) == 0:
+                continue
+
+            icon = desc[icon_index].find_next('img')
+
+            table_traits.append({
+                "name": desc[name_index].text.strip(),
+                "icon": "https://ck3.paradoxwikis.com" + icon['src'],
+                "effects": parse_effects(trait[effects_index]),
+                "description": desc[desc_index].text.strip()
+            })
+    return table_traits
+
 
 traits = []
+
+traits.extend(personality_traits(PERSONALITY))
 for t_name in NORMALS:
-    print(t_name)
-    table_title = soup.find(id=t_name)
+    table_title = SOUP.find(id=t_name)
     table = table_title.find_next("tbody")
     traits.extend(regular_table(table))
 
